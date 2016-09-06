0d33ff33627a3b621ecdfdfa3dc460498a45e091
Vincent <vincentt117@gmail.com>
2016-09-06 14:57:41 -0400

    Email change

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -16,10 +16,10 @@ $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 	
 // Create the email and send the message
-$to = 'vincentt117@gmail.com'; 
+$to = 'v.teng@mail.utoronto.ca'; 
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@yourdomain.com\n"; 
+$headers = "From: https://vincentt117.github.io/\n"; 
 $headers .= "Reply-To: $email_address";	
 mail($to,$email_subject,$email_body,$headers);
 return true;			
