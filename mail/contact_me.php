a6a1db93f88ef45c325a562f0619a2dfacda1867
Vincent <vincentt117@gmail.com>
2016-09-07 10:07:39 -0400

    Added favicon and copy right

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,11 +1,4 @@
 <?php
-echo "export SENDGRID_API_KEY=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" > sendgrid.env
-echo "sendgrid.env" >> .gitignore
-source ./sendgrid.env
-
-require("path/to/sendgrid-php/sendgrid-php.php");
-$apiKey = getenv('SENDGRID_API_KEY');
-$sg = new \SendGrid($apiKey);
 
 $response = $sg->client->suppressions()->bounces()->get();
 // Check for empty fields
@@ -25,7 +18,6 @@ $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 	
 // Create the email and send the message
-$from = 
 $to = new SendGrid\Email(null,'v.teng@mail.utoronto.ca'); 
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
