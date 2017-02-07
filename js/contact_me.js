d24557d5ff14157e41c0b88a998b4721b24a06c4
Vincent Teng <vincent@Vincents-MacBook-Pro.local>
2017-02-06 21:59:29 -0500

    Contact

--- js/contact_me.js
+++ js/contact_me.js
@@ -1,5 +1,4 @@
 $(function() {
-    
 
     $("#contactForm input,#contactForm textarea").jqBootstrapValidation({
         preventSubmit: true,
@@ -22,7 +21,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "",
+                url: "././mail/contact_me.php",
                 type: "POST",
                 data: {
                     name: name,
--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,6 +1,4 @@
 <?php
-
-$response = $sg->client->suppressions()->bounces()->get();
 // Check for empty fields
 if(empty($_POST['name'])  		||
    empty($_POST['email']) 		||
@@ -18,10 +16,11 @@ $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 	
 // Create the email and send the message
+$to = 'vincentt117@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: https://vincentt117.github.io/\n"; 
+$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";	
-mail('vincentt117gmail.com',$email_subject,$email_body,$headers);
+mail($to,$email_subject,$email_body,$headers);
 return true;			
 ?>
